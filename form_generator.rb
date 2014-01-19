2c73cd6a2e4f1d93890eac8c5d87f78a1bc7cae6
Sung Kim <jstkim7@gmail.com>
2014-01-18 21:46:18 -0500

    Complete v.1

--- form_generator.rb
+++ form_generator.rb
@@ -4,18 +4,29 @@ object_name = gets.chomp
 puts "Number of fields: "
 field_count = gets.chomp.to_i
 fields = []
-field_type = []
+field_types = []
 
 puts "Field names and types"
 field_count.times do
   puts "Name: "
   fields << gets.chomp
   puts "Type: "
-  field_type << gets.chomp
+  field_types << gets.chomp
+end
+
+field_strings = []
+
+fields.each_with_index do |f, i|
+  field_strings << <<-fs
+<p>
+  <%= f.label :#{fields[i]} %></br>
+  <%= f.#{field_types[i]} :#{fields[i]} %>
+</p>
+fs
 end
 
 form = <<-form
-<%= form_for @#{object_name} %>
+<%= form_for @#{object_name} do |f| %>
 
 <p>
 <% if @#{object_name}.errors.any? %>
@@ -29,7 +40,10 @@ form = <<-form
 <% end %>
 </p>
 
-
+#{field_strings.join("\n")}
+<p>
+  <%= f.submit %>
+</p>
 
 <% end %>
 form
