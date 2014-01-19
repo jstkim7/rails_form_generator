a10d87eb544f86947aab67028762d30a6eeec85b
Sung Kim <jstkim7@gmail.com>
2014-01-18 21:22:00 -0500

    Crappy first version. Very fragile

---
+++ form_generator.rb
@@ -0,0 +1,23 @@
+puts "Object count: "
+object_count = gets.chomp.to_i
+objects = []
+
+puts "Object names: "
+object_count.times do
+  objects << gets.chomp
+end
+
+puts "Number of fields: "
+field_count = gets.chomp.to_i
+fields = []
+field_type = []
+
+puts "Field names and types"
+
+field_count.times do
+  puts "Name: "
+  fields << gets.chomp
+  puts "Type: "
+  field_type << gets.chomp
+end
+
